64a73d78b038cf5709a8bd4ecebe60ec048fc635
irinalauritto <irina.lauritto@ingenieria.uner.edu.ar>
2024-10-24 11:09:39 -0300

    Se sube calculo de la frecuencia cardíaca

--- firmware/examples/ProyectoFinalIrina/main/ProyectoFinalIrina.c
+++ firmware/examples/ProyectoFinalIrina/main/ProyectoFinalIrina.c
@@ -44,31 +44,10 @@ TaskHandle_t calcularParametrosECGTaskHandle = NULL;
 
 #define CONFIG_BLINK_PERIOD 500
 #define LED_BT	            LED_1
-#define BUFFER_SIZE         256
+#define BUFFER_SIZE         500
 #define SAMPLE_FREQ	        220
 /*==================[internal data definition]===============================*/
-float ecg[] = {
-     76,  76,  77,  77,  76,  83,  85,  78,  76,  85,  93,  85,  79,
-     86,  93,  93,  85,  87,  94,  98,  93,  87,  95, 104,  99,  91,
-     93, 102, 104,  99,  96, 101, 106, 102,  96,  97, 104, 106,  97,
-     94, 100, 103, 101,  91,  95, 103, 100,  94,  90,  98, 104,  94,
-     87,  93,  99,  97,  87,  86,  96,  98,  90,  83,  90,  96,  89,
-     81,  80,  87,  92,  82,  78,  84,  89,  80,  72,  78,  82,  82,
-     73,  72,  81,  82,  79,  69,  77,  82,  81,  76,  68,  78,  80,
-     76,  73,  78,  82,  82,  75,  72,  86,  84,  78,  76,  85,  95,
-     88,  81,  83,  93,  90,  86,  83,  88,  93,  86,  82,  82,  92,
-     89,  82,  82,  88,  94,  84,  82,  90,  98,  94,  87,  91,  95,
-     98,  93,  90,  97, 104, 105,  96,  93, 107, 116, 118, 127, 148,
-    181, 208, 231, 252, 241, 198, 139,  76,  43,  32,  29,  42,  65,
-     86,  90,  88,  93, 101, 107, 102,  98, 103, 110, 104,  98,  99,
-    107, 109,  96,  95, 103, 107, 102,  95,  95, 102, 105,  94,  94,
-    102, 102,  99,  94,  96, 102,  99,  90,  92, 100, 102,  95,  90,
-     98, 104,  97,  89,  94, 102, 103,  97,  93, 100, 105, 102,  93,
-     97, 104, 104, 100,  96, 108, 111, 104,  99, 101, 108, 102,  96,
-     97, 104, 104,  97,  89,  91, 100,  91,  81,  79,  85,  86,  73,
-     69,  75,  79,  75,  68,  68,  76,  76,  69,  67,  74,  81,  77,
-     71,  72,  82,  82,  76,  77,  76,  76,  75
-};
+float ecg[BUFFER_SIZE];
 static float ecg_filt[BUFFER_SIZE];
 static float ecg_fft[BUFFER_SIZE/2];
 static float ecg_filt_fft[BUFFER_SIZE/2];
@@ -80,8 +59,13 @@ uint8_t datoConversionAD;
 bool BRADICARDIA = false;
 bool TAQUICARDIA = false;
 
+uint8_t frecuenciaCardiaca;
+
+// Limites en bpm
 uint8_t limiteTaquicardia = 90;
 uint8_t limiteBradicardia = 60;
+
+uint8_t i = 0;
 /*==================[internal functions declaration]=========================*/
 
 void funcTimerECG(void* param){
@@ -95,26 +79,71 @@ static void adquirirProcesarECG(void *pvParameter){
     	ulTaskNotifyTake(pdTRUE, portMAX_DELAY);    
         AnalogInputReadSingle(CH1, &datoConversionAD);
         
-        // Aplicar filtros ? FFT task aplica los filtros antes de enviar el ecg y trabaja con un tamaño de señal definido.
-
 		UartSendString(UART_PC, (char*)UartItoa(datoConversionAD, 10));
 		UartSendString(UART_PC, "\r");
+        
+        if(i<BUFFER_SIZE)
+        {
+            ecg[i] = datoConversionAD;
+            i++;
+        }
+        else
+        {
+            i = 0;
+            ecg[i] = datoConversionAD;
+            i++;
+        }
 
-        vTaskNotifyGiveFromISR(calcularParametrosECGTaskHandle, pdFALSE);
+        if(i = BUFFER_SIZE)
+        {
+            vTaskNotifyGiveFromISR(calcularParametrosECGTaskHandle, pdFALSE);
+        }
+
     }
 }
 
 static void calcularParametrosECG(void *pvParameter){
     while(true)
     {
-        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);    
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        uint8_t umbralVoltaje = 200;  
 
         // Cálculo de frecuencia cardíaca
+        uint8_t contadorQRS = 0;
+
+        int j = 0;
+        while (j<BUFFER_SIZE)
+        {
+            if(ecg[j]>umbralVoltaje && contadorQRS<2)
+            {
+                contadorQRS++;
+                j = j+50;
+            }
+
+        }
+        
+        frecuenciaCardiaca = contadorQRS/BUFFER_SIZE; // ¿Como relacionar con el tiempo? cada 5miliseg 1 muestra. En 2,5 segundos se levanta el vector
+        frecuenciaCardiaca = frecuenciaCardiaca*((1/2.5)*60);
 
         // Comparación con taquicardia
-
+        if(frecuenciaCardiaca>limiteTaquicardia)
+        {
+            TAQUICARDIA = true;
+        }
+        else
+        {
+            TAQUICARDIA = false;
+        }
         // Comparación con bradicardia
-
+        if(frecuenciaCardiaca<limiteBradicardia)
+        {
+            BRADICARDIA = true;
+        }
+        else
+        {
+            BRADICARDIA = false;
+        }
     }
 }
 
@@ -158,8 +187,6 @@ void read_data(uint8_t * data, uint8_t length){
 /*==================[external functions definition]==========================*/
 void app_main(void){
 
-    // frecuencia de muestreo de 200Hz
-    // Levantar el ECG es hacer una conversión AD
     timer_config_t timerECG = {
         .timer = TIMER_A,
         .period = RETARDO_ECG,
